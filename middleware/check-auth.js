8b8e891aaf39c2df052b4b169db66d6560fdada8
kskondov <kskondov@gmail.com>
2019-03-01 22:12:47 +0200

    cookies and persisted login

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,5 +1,5 @@
 export default function (context) {
-  if (process.client) {
-    context.store.dispatch('initAuth')
-  }
+  //if (process.client) {
+    context.store.dispatch('initAuth', context.req)
+  //}
 }
